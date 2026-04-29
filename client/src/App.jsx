95d90cb047b762342f93879e9a594fba34870835
KOZYR001 <Asmorch11122007@yandex.ru>
2026-04-29 18:34:53 +0400

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { Routes, Route } from 'react-router-dom'
+import LoginLanding from './pages/LoginLanding'
+import { Layout } from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import Employees from './pages/Employees'
+import Attendance from './pages/Attendance'
+import Leave from './pages/Leave'
+import Payslips from './pages/Payslips'
+import Settings from './pages/Settings'
+import PrintPayslip from './pages/PrintPayslip'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={ <LoginLanding/> }/>
+        <Route element={ <Layout /> }>
+          <Route path="/dashboaard" element={ <Dashboard/> }/>
+          <Route path="/employees" element={ <Employees/> }/>
+          <Route path="/attendance" element={ <Attendance/> }/>
+          <Route path="/leave" element={ <Leave/> }/>
+          <Route path="/payslips" element={ <Payslips/> }/>
+          <Route path="/settings" element={ <Settings/> }/>
+        </Route>
+        <Route path="/print/payslips/:id" element={ <PrintPayslip/> }/>
+        <Route path="*" element={<Navigate to="dashboard" replace/>}/>        
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+  return (
+    <div className="flex h-screen bg-lineear-to-br from-slate-50 via-white to-indigo-50/30">
+        <p>Sidebar</p>
+        <main>
+            <div>
+                <Outlet />
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Layout
